7450509927a85ce57d5b8e1c524a9c4d2f3e94e3
gjc2118 <gjc2118@gmail.com>
2015-06-28 16:52:58 -0700

    real sweet

--- db/migrate/02_create_artists.rb
+++ db/migrate/02_create_artists.rb
@@ -1,2 +1,8 @@
 class CreateArtists < ActiveRecord::Migration
+    def change
+    create_table :Artists do |t|
+      t.timestamps  null: false
+      t.string :name
+    end
+  end
 end
--- db/migrate/03_create_genres.rb
+++ db/migrate/03_create_genres.rb
@@ -1,2 +1,8 @@
 class CreateGenres < ActiveRecord::Migration
+    def change
+    create_table :Genres do |t|
+      t.timestamps  null: false
+      t.string :name
+    end
+  end
 end
